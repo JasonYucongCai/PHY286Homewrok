7a1e4d5153f6112d9a9b2d4662cb3ea98a5efabb
Pallas Y Cai <JasonYucongCai@users.noreply.github.com>
2017-02-04 20:13:17 -0500

    Update HW01_Problem_04.m

--- HW01_Problem_04.m
+++ HW01_Problem_04.m
@@ -1,16 +1,25 @@
 function[]=HW01_Problem_04(n)
-% using the modern definition include the initial integer 0.
+% The function accepts a single integer (n) as input and returns the nth 
+% Fibonacci number using the closed form expression (called "Binet's 
+% formula") in Wikipedia's page. Make the function print the %error
+% between the closed-form computed value and the "true" nth Fibonacci 
+% number in the series.
+%
+% Using the modern definition include the initial integer 0.
+% The input must be itegers.
 % the program will return the n't Fibonacci number.
 %Yuocng Cai
 
 %-------------------------------------------------------------------------
-if nargin ~=2 %nargin -- new concept! see help nargin
-  disp('Need exactly two inputs. See help');
+if nargin ~=1 %nargin -- new concept! see help nargin
+  disp('Need exactly one inputs. See help');
     %error('Need exactly two inputs. See help');
   return;
 end
+%Input comfirm, Structure comfirm
 %-------------------------------------------------------------------------
 
 x=1/sqrt(5)*(   ((1+sqrt(5))/2)^n - ((1-sqrt(5))/2)^n  );
 format short;
 fprintf('the %i Fibonacci number is  %.f\n',n,x);
+%-------------------------------------------------------------------------
