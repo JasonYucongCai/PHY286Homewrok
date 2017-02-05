5980c7e5336f0ae1c6c250224fb5d07660d254bb
Pallas Y Cai <JasonYucongCai@users.noreply.github.com>
2017-02-04 19:31:52 -0500

    Update HW01_Problem_03.m

--- HW01_Problem_03.m
+++ HW01_Problem_03.m
@@ -1,7 +1,17 @@
 function[]=HW01_Problem_03(n)
-% using the modern definition include the initial integer 0.
-% the program will return the n't Fibonacci number.
-%Yuocng Cai
+% The function that takes a single integer (n) as input and returns the 
+% nth Fibonacci number without using any the function.
+%
+% The function using the modern definition include the initial integer 0.
+% the program will return the n't Fibonacci number. n must be an integer.
+% Yuocng Cai
+if nargin ~=1 %nargin -- new concept! see help nargin
+  disp('Need exactly one inputs. See help');
+    %error('Need exactly two inputs. See help');
+  return;
+end
+%Input comfirm, Structure comfirm
+%-------------------------------------------------------------------------
 switch n
     case 0
         x=0;
@@ -10,13 +20,14 @@ switch n
     otherwise
         x=1;
         y=0;
-        
+%-------------------------------------------------------------------------      
         if n<0
             new=(-n);
         else
             new=n;
         end
-        
+%resolve the negative integer
+%-------------------------------------------------------------------------
         for i=1:1:(new-1)
             %y=a{n-1}
             z=x;% store the an;
@@ -26,7 +37,8 @@ switch n
         
        % fprintf('at round %i, the value of x=%i y=%i\n ',i+1,x,y);
         end
-         
+% find the absolute value of the Fibonacci number.
+%-------------------------------------------------------------------------
         if n<0
             switch 1
                
@@ -39,8 +51,10 @@ switch n
             end
        
         end
-       
-        
+% find the sign of the Fibonacci number
+%-------------------------------------------------------------------------        
 end 
 
 fprintf('the %i Fibonacci number is  %i\n',n,x);
+% print out the number
+%-------------------------------------------------------------------------
