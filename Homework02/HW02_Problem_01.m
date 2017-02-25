7843feb9ac723b6b448141f736c47cb136b7de57
Pallas Y Cai <JasonYucongCai@users.noreply.github.com>
2017-02-25 13:54:05 -0500

    Update HW02_Problem_01.m

--- Homework02/HW02_Problem_01.m
+++ Homework02/HW02_Problem_01.m
@@ -2,13 +2,28 @@
 function[]=HW02_Problem_01(N0,a,b,T,n)
 
 %check
+%-------------------------------------------------------------------------
+if nargin ~=5 %nargin -- new concept! see help nargin
+  disp('Need exactly five inputs. See help');
+    %error('Need exactly two inputs. See help');
+  return;
+end
+%Input comfirm, Structure comfirm
+%-------------------------------------------------------------------------
+if n<0 || T<0 || N0<0
+    error('check your input, the population, time, or step could not be negative.');
+end
+%Input comfirm, legal adjusting.
+%-------------------------------------------------------------------------
 
-
-t=linspace(1,n,n);
+xaxis=linspace(1,n,n);
 delta=T/n;
-
-%Rk
 N(1)=N0;
+
+
+%setup
+%-------------------------------------------------------------------------
+
 
 for i=1:n;
 
@@ -16,8 +31,13 @@ for i=1:n;
 Nrk(i)=
 t=
 
+
+
+%loop RK
+%-------------------------------------------------------------------------
+
+
 %calculate
 
 
 end
-
