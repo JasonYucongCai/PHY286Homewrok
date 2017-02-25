13ab13a36a23d2c120bdc63e11ba36eb547c8295
Max7777 <kreidemb@miamioh.edu>
2017-02-25 15:27:34 -0500

    Add files via upload

--- Homework02/HW02_Problem_02.m
+++ Homework02/HW02_Problem_02.m
@@ -1,1 +1,140 @@
+function [x_normal, y_normal, x_airdrag, y_airdrag, x_density, y_density, Py] = HW02_Problem_02(v0, theta, dt, B, m)
+%function [x_normal, y_normal, x_airdrag, y_airdrag, x_density, y_density, Py] = HW02_Problem_02(v0, theta, dt, B, m)
+%
+%This function displays the trajectory of an object in three ways.  First,
+%without taking into account air resistance or density.  Second, by taking
+%into account only air resistance.  Third, by taking into account both air
+%resistance and density.
+%
+%Input: v0: the initial velocity of the object (m/s)
+%       theta: the angle at which the object is to be fired (degrees)
+%       dt: time-step
+%       B: drag coefficient
+%       m: mass of the object
+%
+%Output: x_normal: the x position of the object fired without taking into
+%                  account air resistance or density
+%        y_normal: the y position of the object fired without taking into
+%                  account air resistance or density
+%        x_airdrag: the x position of the object fired taking into account
+%                   only air resistance
+%        y_airdrag: the y position of the object fired taking into account
+%                   only air resistance
+%        x_density: the x position of the object fired taking into account
+%                   both air resistance and density
+%        y_density: the y position of the object fired taking into account
+%                   both air resistance and density
+%        Py: calculates density as a function of height (y_density)
+                  
+%-----------------------initial values-------------------------------------
 
+%position
+x_normal(1) = 0;
+y_normal(1) = 0;
+
+x_airdrag(1) = 0;
+y_airdrag(1) = 0;
+
+x_density(1) = 0;
+y_density(1) = 0;
+
+%velocity
+vx_normal(1) = v0.*cosd(theta);
+vy_normal(1) = v0.*sind(theta);
+
+vx_airdrag(1) = v0.*cosd(theta);
+vy_airdrag(1) = v0.*sind(theta);
+
+vx_density(1) = v0.*cosd(theta);
+vy_density(1) = v0.*sind(theta);
+
+%constants
+g = 9.8;
+y_0 = 10000; %approximate value from book, calculated as (kB*T/m*g)
+
+%pressure
+p_0 = 1.225; %approximate value of air density at sea level
+
+Py(1) = p_0.*exp(-y_density(1)./y_0);
+
+%--------------------------compute-----------------------------------------
+
+%projectile with neither drag nor density
+counter_n = 1;
+while y_normal(counter_n) >= 0
+   
+    %velocity
+    vx_normal(counter_n + 1) = vx_normal(counter_n) + dt.*0;
+    vy_normal(counter_n + 1) = vy_normal(counter_n) + dt.*(-g);
+    
+    %position
+    x_normal(counter_n + 1) = x_normal(counter_n) + dt.*(vx_normal(counter_n));
+    y_normal(counter_n + 1) = y_normal(counter_n) + dt.*(vy_normal(counter_n));
+   
+    %increment counter_n
+    counter_n = counter_n + 1;
+end
+
+%projectile with drag only
+counter_d = 1;
+while y_airdrag(counter_d) >= 0
+   
+    %velocity
+    vx_airdrag(counter_d + 1) = vx_airdrag(counter_d) + dt.*0 + (-B./m).*vx_airdrag(counter_d).*sqrt(vx_airdrag(counter_d).*vx_airdrag(counter_d) + vy_airdrag(counter_d).*vy_airdrag(counter_d)).*dt;
+    vy_airdrag(counter_d + 1) = vy_airdrag(counter_d) + dt.*(-g) + (-B./m).*vy_airdrag(counter_d).*sqrt(vx_airdrag(counter_d).*vx_airdrag(counter_d) + vy_airdrag(counter_d).*vy_airdrag(counter_d)).*dt;
+    
+    %position
+    x_airdrag(counter_d + 1) = x_airdrag(counter_d) + dt.*(vx_airdrag(counter_d));
+    y_airdrag(counter_d + 1) = y_airdrag(counter_d) + dt.*(vy_airdrag(counter_d));
+    
+    %increment counter_d
+    counter_d = counter_d + 1;
+end
+
+%projectile with both drag and density
+counter_dd = 1;
+while y_density(counter_dd) >= 0
+    %solve for density
+    Py(counter_dd) = p_0.*exp(-y_density(counter_dd)./y_0);
+    
+    %velocity
+    vx_density(counter_dd + 1) = vx_density(counter_dd) + dt.*0 + (-B./m).*(Py(counter_dd)./p_0).*vx_density(counter_dd).*sqrt(vx_density(counter_dd).*vx_density(counter_dd) + vy_density(counter_dd).*vy_density(counter_dd)).*dt;
+    vy_density(counter_dd + 1) = vy_density(counter_dd) + dt.*(-g) + (-B./m).*(Py(counter_dd)./p_0).*vy_density(counter_dd).*sqrt(vx_density(counter_dd).*vx_density(counter_dd) + vy_density(counter_dd).*vy_density(counter_dd)).*dt;
+    
+    %position
+    x_density(counter_dd + 1) = x_density(counter_dd) + dt.*(vx_density(counter_dd));
+    y_density(counter_dd + 1) = y_density(counter_dd) + dt.*(vy_density(counter_dd));
+    
+    %increment counter_dd
+    counter_dd = counter_dd + 1;
+end
+
+%compute range of projectile
+r_normal = - (y_normal(end - 1) / y_normal(end));
+range_normal = (x_normal(end - 1) + r_normal*x_normal(end)) / (r_normal + 1);
+
+r_airdrag = - (y_airdrag(end - 1) / y_airdrag(end));
+range_airdrag = (x_airdrag(end - 1) + r_airdrag*x_airdrag(end)) / (r_airdrag + 1);
+
+r_density = - (y_density(end - 1) / y_density(end));
+range_density = (x_density(end - 1) + r_density*x_density(end)) / (r_density + 1);
+
+%----------------------------plot------------------------------------------
+
+plot(x_normal,y_normal,'r.-', x_airdrag, y_airdrag,'b.-', x_density, y_density, 'g.-');
+legend('Without Drag or density', 'With Drag only', 'With Drag and Density');
+xlim([0 inf]);
+ylim([0 inf]);
+xlabel('X (m)');
+ylabel('Y (m)');
+title('Projectile Motion');
+  
+%---------------------------display other info----------------------------- 
+
+disp(' ');
+fprintf('The projectile that did not take into account drag or density traveled approximately %.3f meters.', range_normal); 
+disp(' ');
+fprintf('The projectile that only took into account drag traveled approximately %.3f meters.', range_airdrag); 
+disp(' ');
+fprintf('The projectile that took into account both drag and density traveled approximately %.3f meters.', range_density);  
+ 
--- Homework02/HW02_Problem_03.m
+++ Homework02/HW02_Problem_03.m
@@ -1,1 +1,70 @@
+function [prey, predator, time] = HW03_Problem_03(alpha, beta, delta, gamma, prey_0, predator_0, dt, t_max, choice)
+%function [prey, predator, time] = HW03_Problem_03(alpha, beta, delta, gamma, prey_0, predator_0, dt, t_max, choice)
+%
+%This function does 2 things.  First, it plots the number of prey and
+%predator as a function of time.  Second, it plots predator vs prey as a
+%phase plot
+%
+%Input: alpha: a constant
+%       beta: a constant
+%       delta: a constant
+%       gamma: a constant
+%       prey_0: initial number of prey present
+%       predator_0: initial number of predators present
+%       dt: time-step
+%       t_max: the time interval over which the calculations will be made
+%       choice: should be '1' for the Euler method, '2' for R-K method, any
+%               other value results in a error
+%
+%Output: prey: number of prey present
+%        predator: number of predators present
+%        time: a vector containing times at which the number of prey and
+%              predators was evaluated
+
+%---------------------error check------------------------------------------
+
+if choice ~= 1 && choice ~= 2
+    error('This function will use either Eulers method or the R-K method.  Please input "1" for Eulers method or "2" for the R-K method.');
+end
+
+%---------------------initial values---------------------------------------
+
+prey(1) = prey_0;
+predator(1) = predator_0;
+
+time = [0:dt:t_max];
+
+%--------------------calculate---------------------------------------------
+
+for count = 1:length(time) - 1
+    
+    %Euler method
+    if choice == 1
+    prey(count + 1) = prey(count) + dt.*(alpha.*prey(count) - beta.*prey(count).*predator(count));
+    predator(count + 1) = predator(count) + dt.*(delta.*prey(count).*predator(count) - gamma.*predator(count));
+    
+    %R-K method
+    else
+        prey_prime = prey(count) + .5.*dt;
+        predator_prime = predator(count) + .5.*dt.*(delta.*prey(count).*predator(count) - gamma.*predator(count));
+        
+        prey(count + 1) = prey(count) + dt.*(alpha.*prey_prime - beta.*prey_prime.*predator_prime);
+        predator(count + 1) = predator(count) + dt.*(delta.*prey_prime.*predator_prime - gamma.*predator_prime);
+    end
+    
+end
+
+%---------------------plot-------------------------------------------------
+
+subplot(2,1,1); plot(time, prey, time, predator);
+title('Prey & Predator vs. Time');
+xlabel('Time (dimensionless)');
+ylabel('Population of Prey & Predator');
+legend('Prey','Predator');
+
+subplot(2,1,2); plot(prey, predator);
+title('Phase Plot, Predator vs. Prey');
+xlabel('Prey');
+ylabel('Predator');
+
 
